ac96989c7e039f6ff40f98eac9a60c4577360fd6
강주희 <claudiajkang@gmail.com>
2020-09-30 22:35:18 +0900

    Define OAuth2AuthorizationCode model

--- app/model/auth.py
+++ app/model/auth.py
@@ -1,11 +1,29 @@
 from app import db
 from sqlalchemy.orm import relationship
+from authlib.integrations.sqla_oauth2 import (
+    OAuth2ClientMixin,
+    OAuth2AuthorizationCodeMixin
+)
 from sqlalchemy import Column, Integer, ForeignKey
-from authlib.integrations.sqla_oauth2 import OAuth2ClientMixin
-
 
 class OAuth2Client(db.Model, OAuth2ClientMixin):
     __tablename__ = 'oauth2_client'
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    user_id = Column(
+        Integer,
+        ForeignKey('user.id', ondelete='CASCADE')
+    )
+
+    user = relationship('User')
+
+
+class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
+    __tablename__ = 'oauth2_code'
 
     id = Column(
         Integer,
