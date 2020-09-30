481e27461df238c2af955b7b3e3351321cd76c82
강주희 <claudiajkang@gmail.com>
2020-09-30 23:04:12 +0900

    Add RefreshTokenGrant class

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,7 +1,7 @@
 from app import db
 from app.model.user import User
 from authlib.oauth2.rfc6749 import grants
-from app.model.auth import OAuth2AuthorizationCode
+from app.model.auth import OAuth2Token, OAuth2AuthorizationCode
 
 
 class AuthorizationCodeGrant(grants.AuthorizationCodeGrant):
@@ -52,3 +52,19 @@ class PasswordGrant(grants.ResourceOwnerPasswordCredentialsGrant):
 
         if user is not None and user.check_password(password):
             return user
+
+
+class RefreshTokenGrant(grants.RefreshTokenGrant):
+    def authenticate_refresh_token(self, refresh_token):
+        token = OAuth2Token.query.filter_by(refresh_token=refresh_token).first()
+
+        if token and token.is_refresh_token_active():
+            return token
+
+    def authenticate_user(self, credential):
+        return User.query.get(credential.user_id)
+
+    def revoke_old_credential(self, credential):
+        credential.revoked = True
+        db.session.add(credential)
+        db.session.commit()
