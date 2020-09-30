e434134bed5165de780cf6bd72d0692d61857f40
강주희 <claudiajkang@gmail.com>
2020-09-30 22:38:30 +0900

    Define OAuth2Token model

--- app/model/auth.py
+++ app/model/auth.py
@@ -1,8 +1,10 @@
+import time
 from app import db
 from sqlalchemy.orm import relationship
 from authlib.integrations.sqla_oauth2 import (
     OAuth2ClientMixin,
-    OAuth2AuthorizationCodeMixin
+    OAuth2AuthorizationCodeMixin,
+    OAuth2TokenMixin
 )
 from sqlalchemy import Column, Integer, ForeignKey
 
@@ -36,3 +38,27 @@ class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
     )
 
     user = relationship('User')
+
+
+class OAuth2Token(db.Model, OAuth2TokenMixin):
+    __tablename__ = 'oauth2_token'
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    user_id = Column(
+        Integer,
+        ForeignKey('user.id', ondelete='CASCADE')
+    )
+
+    user = relationship('User')
+
+    def is_refresh_token_active(self):
+        if self.revoked:
+            return False
+
+        expires_at = self.issued_at + self.expires_in * 2
+
+        return expires_at >= time.time()
