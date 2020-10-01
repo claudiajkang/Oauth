9b15930e2bc724a278d6d6a41641a74211891fb5
강주희 <claudiajkang@gmail.com>
2020-10-01 10:29:14 +0900

    Add authorize route

---
+++ app/route/oauth.py
@@ -0,0 +1,32 @@
+from app import app
+from app.model.user import User
+from app.oauth2 import authorization
+from authlib.oauth2 import OAuth2Error
+from app.util.helpers import current_user
+from flask import request, url_for, render_template, redirect
+
+
+@app.route('/oauth/authorize', methods=['GET', 'POST'])
+def authorize():
+    user = current_user()
+
+    if not user:
+        return redirect(url_for('home', next=request.url))
+
+    if request.method == "GET":
+        try:
+            grant = authorization.validate_consent_request(end_user=user)
+        except OAuth2Error as error:
+            return error.error
+        return render_template('authorize.html', user=user, grant=grant)
+
+    if not user and 'username' in request.form:
+        username = request.form.get('username')
+        user = User.query.filter_by(username=username).first()
+
+    if request.form['confirm']:
+        grant_user = user
+    else:
+        grant_user = None
+
+    return authorization.create_authorization_response(grant_user=grant_user)
